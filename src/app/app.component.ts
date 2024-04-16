ee398de7b3e2a5ba4efa30e7c78d089eb80f2a36
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-04-16 09:37:15 +0200

    feat: adjust app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IconGeneratorComponent } from './icon-generator/icon-generator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IconGeneratorComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'test-app';
